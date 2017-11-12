112e5510b7b429f02f79a2032c6272e5e0892a33
kashiamoghvardhan@gmail.com <kashiamoghvardhan@gmail.com>
2017-11-12 20:12:07 +0530

    alerts on repeated requests

--- server/controllers/ControllerFamily.js
+++ server/controllers/ControllerFamily.js
@@ -15,13 +15,32 @@ module.exports.meetReqFam = function(req, res) {
     pid:req.body.pid,
     pname:req.body.pname
   }
-  connection.query('update  dependants set Reason =? where Name =?', [info.reason,info.name], function(err,result){
+
+  connection.query('select Status from dependants where Name=?', [info.name], function(err,result){
    if(err) {
      console.log(err);
      res.send({success: false});
    }
    else {
-     res.send({success: true});
+     if(result.Status=="Approved"||result.Status=="Pending")
+     {
+       connection.query('update  dependants set Reason =? where Name =?', [info.reason,info.name], function(err,result){
+        if(err) {
+          console.log(err);
+          res.send({success: false});
+        }
+        else {
+          res.send({success: true});
+        }
+       });
+     }
+     else {
+       console.log("sladjfkslafjj");
+       res.send({stat:"Previous request still pending. Your new request is not accepted. Please wait for few more days"});
+     }
    }
   });
+
+
+
   }
